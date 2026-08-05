ccfe1856b7ebb635572f24ac938eec7bc87c5fba
Nikhil-Wadatkar-Repo <nikhil.wadatkar4693@gmail.com>
2026-08-05 16:59:09 +0530

    Added concurrent updates scenario project

---
+++ src/main/java/com/mco/concurrentupdateservice/ConcurrentUpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mco.concurrentupdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentUpdateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentUpdateServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mco/concurrentupdateservice/ConcurrentUpdateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mco.concurrentupdateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentUpdateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
